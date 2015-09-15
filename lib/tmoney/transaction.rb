74da29dcc99e73be78d46d428b32bfcc27273e90
buo <buo@users.noreply.github.com>
2015-09-16 03:03:56 +0900

    Add the Transaction constructor

--- lib/tmoney/transaction.rb
+++ lib/tmoney/transaction.rb
@@ -2,6 +2,12 @@ module Tmoney
   class Transaction
     attr_accessor :datetime, :category, :payee, :amount
 
+    def initialize(args={})
+      args.each_pair do |key, value|
+        send("#{key}=", value)
+      end
+    end
+
     def to_s
       "#{@datetime.strftime('%Y-%m-%d %H:%M:%S')} #{@category} #{@payee} #{@amount}"
     end
